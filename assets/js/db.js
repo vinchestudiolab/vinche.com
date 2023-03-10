9867ac411bddb96bb7e18df2cb1d51b4b1db6428
vinchestudiolab <info@vinche.com>
2023-03-09 21:59:51 -0300

    Proyecto 9 marzo 2023

---
+++ assets/js/db.js
@@ -0,0 +1,114 @@
+let array = [
+	{ 
+		codigo:"niketextil", 
+		fotos:"4", 
+		titulo: "Nike originales téxtiles", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Nike", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: Vives Vidal · Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"arquitectura", 
+		fotos:"8", 
+		titulo: "Presentaciones de proyectos de Arquitectura", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: FADU · UDELAR", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: FADU ·  Montevideo", 
+		programas: "Programas: Archicad, Sketchup, Lumion, Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"solelorenzo", 
+		fotos:"4", 
+		titulo: "Diseño Gráfico prensa", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Sole&Lorenzo", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Ideas Publicitarias ·  Montevideo", 
+		programas: "Programas: Archicad, Sketchup, Lumion, Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"thamato", 
+		fotos:"5", 
+		titulo: "Diseño Gráfico prensa", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Sole&Lorenzo", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Ideas Publicitarias ·  Montevideo", 
+		programas: "Programas: Archicad, Sketchup, Lumion, Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"upc", 
+		fotos:"5", 
+		titulo: "Diseño Gráfico UPC", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Sole&Lorenzo", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Ideas Publicitarias ·  Montevideo", 
+		programas: "Programas: Archicad, Sketchup, Lumion, Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"victory", 
+		fotos:"3", 
+		titulo: "nike originales téxtiles", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Nike", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: Vives Vidal Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"poster", 
+		fotos:"4", 
+		titulo: "Ideas+ originales téxtiles", 
+		descripcion: "Cada original de diseño textil y las distintas gráficas han sido creado a partir de una variedad de formas geométricas y colores, con formas que refuerzan el movimiento.<br />Este diseño de originales téxtiles a partir de formas geométricas dinámicas.", 
+		cliente: "Cliente: Nike", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: Vives Vidal Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"nikegrafico", 
+		fotos:"7", 
+		titulo: "titulo intimacherry", 
+		descripcion: "descripcion intimacherry", 
+		cliente: "Cliente: intimacherry", 
+		anio: "Año: 2020", 
+		estudio: "Estudio: Vives Vidal Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "resumen intimacherry"  
+	},
+	{ 
+		codigo:"galata", 
+		fotos:"3", 
+		titulo: "titulo Gálata", 
+		descripcion: "descripcion Gálata", 
+		cliente: "Cliente: Gálata", 
+		anio: "Año: 2020", 
+		estudio: "Estudio: IBCmass Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "resumen intimacherry"  
+	},
+	{ 
+		codigo:"accesibilidad", 
+		fotos:"3", 
+		titulo: "titulo Accesibilidad", 
+		descripcion: "descripcion Accesibilidad", 
+		cliente: "Cliente: Accesibilidad", 
+		anio: "Año: 2020", 
+		estudio: "Estudio: IBCmass Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "resumen intimacherry"  
+	},
+	
+
+];
