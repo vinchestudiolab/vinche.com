31c2eae3c0bef46967cac1552934ce853c25fd9b
vinchestudiolab <info@vinche.com>
2023-04-14 15:30:15 -0300

    Add files via upload

---
+++ assets/js/base.js
@@ -0,0 +1,307 @@
+  
+  
+
+  
+  
+  
+  
+  	// logo -webkit- grid-img getElementsByClassName("example")
+  	window.onscroll = function() { scrollFunction() };
+
+	function scrollFunction() {
+	  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+		document.getElementById("logo").style.width = "30%";
+		//document.querySelectorAll(".grid-img").filter = "grayscale(0%)";
+		//document.getElementsByClassName("grid-img").-webkit-filter = "grayscale(0%)";
+		//let imagenes = document.querySelectorAll("a > img")
+		//imagenes.style.filter = 'grayscale(0%)';
+		
+		//recorre todas las img
+		var el = document.querySelectorAll('img');
+			for(var i=0;i<el.length;i++){
+  			el[i].style.filter = 'grayscale(0%)';
+		}
+		
+		//const note = document.querySelector('.grid-img');
+		//note.style.filter = 'grayscale(0%)';
+
+	  } else {
+		document.getElementById("logo").style.width = "40%";
+		//document.querySelectorAll(".grid-img").filter = "grayscale(100%)";
+		//document.getElementsByClassName("grid-img").-webkit-filter = "grayscale(100%)";
+		//const note = document.querySelector('.grid-img');
+		//note.style.filter = 'grayscale(100%)';
+		
+		//recorre todas las img
+		var el = document.querySelectorAll('img');
+			for(var i=0;i<el.length;i++){
+  			el[i].style.filter = 'grayscale(100%)';
+		}
+
+	  }
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+  	// diseño texto
+	$(document).ready(function() {	
+		//12 proyectos
+	  $(window).on("load scroll", function() {
+		var parallaxElement = $(".product-page-year-text-holder"),
+		  parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+		  for (var i = 0; i < parallaxQuantity; i++) {
+			var currentElement = parallaxElement.eq(i),
+			  windowTop = $(window).scrollTop(),
+			  elementTop = currentElement.offset().top,
+			  elementHeight = currentElement.height(),
+			  viewPortHeight = window.innerHeight * 0.07 - elementHeight * 0.07,
+			  scrolled = windowTop - elementTop + viewPortHeight;
+			currentElement.css({
+			  transform: "translate3d(" + scrolled * -0.3 + "px, 1.8rem, 0)"
+			});
+		  }
+		});
+	  });
+		
+  		//parallax scroll
+	  $(window).on("load scroll", function() {
+		var parallaxElement = $(".category-h1"),
+		  parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+		  for (var i = 0; i < parallaxQuantity; i++) {
+			var currentElement = parallaxElement.eq(i),
+			  windowTop = $(window).scrollTop(),
+			  elementTop = currentElement.offset().top,
+			  elementHeight = currentElement.height(),
+			  viewPortHeight = window.innerHeight * 0.07 - elementHeight * 0.07,
+			  scrolled = windowTop - elementTop + viewPortHeight;
+			currentElement.css({
+			  transform: "translate3d(0," + scrolled * -0.3 + "px, 0)"
+			});
+		  }
+		});
+	  });
+	
+  		//parallax scroll proyectos
+	  $(window).on("load scroll", function() {
+		var parallaxElement = $(".parallax_scroll"),
+		  parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+		  for (var i = 0; i < parallaxQuantity; i++) {
+			var currentElement = parallaxElement.eq(i),
+			  windowTop = $(window).scrollTop(),
+			  elementTop = currentElement.offset().top,
+			  elementHeight = currentElement.height(),
+			  viewPortHeight = window.innerHeight * 0.01 - elementHeight * 0.01,
+			  scrolled = windowTop - elementTop + viewPortHeight;
+			currentElement.css({
+			  transform: "translate3d(0," + scrolled * -0.01 + "px, 0)"
+			});
+		  }
+		});
+	  });
+	  
+	  //parallax scroll proyectos
+	  $(window).on("load scroll", function() {
+		var parallaxElement = $(".parallax_scroll-"),
+		  parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+		  for (var i = 0; i < parallaxQuantity; i++) {
+			var currentElement = parallaxElement.eq(i),
+			  windowTop = $(window).scrollTop(),
+			  elementTop = currentElement.offset().top,
+			  elementHeight = currentElement.height(),
+			  viewPortHeight = window.innerHeight * 0.01 - elementHeight * 0.01,
+			  scrolled = windowTop - elementTop + viewPortHeight;
+			currentElement.css({
+			  transform: "translate3d(0," + scrolled * +0.08 + "px, 0)"
+			});
+		  }
+		});
+	  });
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+ 
+	// proyecto tomamos los valores de get y de db.js
+    function getGET()
+    {
+        // capturamos la url
+        var loc = document.location.href;
+        // si existe el interrogante
+        if(loc.indexOf('?')>0)
+        {
+            // cogemos la parte de la url que hay despues del interrogante
+            var getString = loc.split('?')[1];
+            // obtenemos un array con cada clave=valor
+            var GET = getString.split('&');
+            var get = {};
+            // recorremos todo el array de valores
+            for(var i = 0, l = GET.length; i < l; i++){
+                var tmp = GET[i].split('=');
+                get[tmp[0]] = unescape(decodeURI(tmp[1]));
+            }
+            return get;
+        }
+    }
+    window.onload = function()
+    {
+        
+        // Cogemos los valores pasados por get
+        var valores=getGET();
+        
+        var proyecto = valores['proyecto'];
+        
+        // pagina proyecto
+        if(proyecto !== '')
+        {
+            //recogemos los valores que nos envia la URL en variables para trabajar 
+            // hacemos un bucle para pasar por cada indice del array de valores db.js           
+			var i = 1;
+				array.forEach(object=>{	
+	
+					if(object.codigo == proyecto){
+						
+						document.getElementById('titulo').innerHTML = object.titulo;
+						document.getElementById('descripcion').innerHTML = object.descripcion;
+						document.getElementById('cliente').innerHTML = object.cliente;  
+						document.getElementById('anio').innerHTML = object.anio; 
+						document.getElementById('estudio').innerHTML = object.estudio; 
+						document.getElementById('programas').innerHTML = object.programas; 
+						document.getElementById('resumen').innerHTML = object.resumen; 
+					
+						if(object.fotos == 0){
+							f1.style.display = "none";
+							f2.style.display = "none";
+							f3.style.display = "none";
+							f4.style.display = "none";
+							f5.style.display = "none";
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 1){
+							f2.style.display = "none";
+							f3.style.display = "none";
+							f4.style.display = "none";
+							f5.style.display = "none";
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 2){
+							f3.style.display = "none";
+							f4.style.display = "none";
+							f5.style.display = "none";
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 3){
+							f4.style.display = "none";
+							f5.style.display = "none";
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 4){
+							f5.style.display = "none";
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 5){
+							f6.style.display = "none";
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 6){
+							f7.style.display = "none";
+							f8.style.display = "none";
+							}
+						if(object.fotos == 7){
+							f8.style.display = "none";
+							}									
+					}	
+					i++;
+				});    
+				
+            //alert('Te llamas '+ proyecto);
+            document.getElementById("imagen-00").src="assets/imagenes/" + proyecto + "-00.jpg";
+            document.getElementById("imagen-01").src="assets/imagenes/" + proyecto + "-01.jpg";
+            document.getElementById("imagen-02").src="assets/imagenes/" + proyecto + "-02.jpg";
+            document.getElementById("imagen-03").src="assets/imagenes/" + proyecto + "-03.jpg";
+            document.getElementById("imagen-04").src="assets/imagenes/" + proyecto + "-04.jpg";
+            document.getElementById("imagen-05").src="assets/imagenes/" + proyecto + "-05.jpg";
+            document.getElementById("imagen-06").src="assets/imagenes/" + proyecto + "-06.jpg";
+            document.getElementById("imagen-07").src="assets/imagenes/" + proyecto + "-07.jpg";
+            document.getElementById("imagen-08").src="assets/imagenes/" + proyecto + "-08.jpg";
+
+        }else{
+            // no se ha recibido ningun parametro por GET
+            //document.write("No se ha recibido ningún parámetro");
+            window.location.replace("http://www.vinche.com");
+        }
+    }
+
+
+
+
+
+
+
+	var x = 1;
+	array.forEach(object=>{	
+	
+		 document.getElementById("href-0"+x).href="./proyectos.html?proyecto=" + object.codigo + "";
+		 document.getElementById("imagen-0"+x).src="assets/imagenes/" + object.codigo + "-portada.jpg";
+		 
+		 
+		 document.getElementById('titulo-portada-0'+x).innerHTML = object.tituloportada;
+
+		x++;
+	});
+
+
+
+
+
+
+
+
+		// ancla
+		 $(function(){
+		   $("a.ancla").off().on("click", function(e){
+			  e.preventDefault();
+			  var ancla = $(this).attr("href");
+			  $("html,body").animate({
+				   scrollTop: $(ancla).offset().top
+			  });
+		   });
+	   });
+
+	
+	
+	
+	
+	
---
+++ assets/js/db.js
@@ -0,0 +1,124 @@
+let array = [
+	{ 
+		codigo:"niketextil", 
+		fotos:"4", 
+		tituloportada: "<span>Nike</span> · Diseño Textil <br />Barcelona", 
+		titulo: "Nike originales téxtiles", 
+		descripcion: "El diseño de originales Nike se caracteriza por el uso de patrones geométricos de diferentes formas y tamaños, así como la utilización de colores vibrantes y contrastantes. <br/> <br/>  <br/> Las formas geométricas están inspiradas en el mundo del deporte, con patrones que evocan la velocidad, la fuerza y la precisión. Los colores dinámicos, por su parte, reflejan la energía y la pasión que los deportes despiertan en los atletas y aficionados..", 
+		cliente: "Cliente: Nike", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: Vives Vidal · Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño textil de originales a partir de formas geométricas dinámicas." 
+	},
+	{ 
+		codigo:"arquitectura", 
+		fotos:"8", 
+		tituloportada: "<span>Arquitectura</span> · Diseño y representación<br />Montevideo", 
+		titulo:  "3D & postproducción de proyectos de Arquitectura", 
+		descripcion: "Los proyectos de Arquitectura son el resultado de una combinación de tecnología 3D y postproducción fotográfica. <br/> <br/>  <br/> Esta combinación ha permitido crear diseños que no solo son funcionales y estéticamente atractivos, sino también increíblemente realistas, que permiten representar diferentes opciones y soluciones antes de comprometerme con un diseño final. Pero lo que realmente ha hecho que los proyectos destaquen es la postproducción fotográfica que permiten destacar las características únicas del diseño.", 
+		cliente: "Cliente: FADU · UDELAR", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: FADU ·  Montevideo", 
+		programas: "Programas: Archicad, Sketchup, Lumion, Illustrator, Photoshop", 
+		resumen: "Proyectos de Arquitectura diseño y representación." 
+	},
+	{ 
+		codigo:"solelorenzo", 
+		fotos:"4", 
+		tituloportada: "<span>Sole&Lorenzo</span> · Diseño gráfico<br />Montevideo", 
+		titulo: "Diseño Gráfico prensa", 
+		descripcion: "Para Solé&Lorenzo se ha creado un diseño gráfico que transmite profesionalismo y elegancia características en su soporte papel. <br/> <br/>  El diseño presenta una combinación de colores sobrios y elegantes, como el gris oscuro y el plateado, que se complementan con el uso de fotografías para agregar un toque de sofisticación. Las imágenes de los vehículos se presentan en una disposición organizada y limpia, con detalles precisos que resaltan las características únicas de cada modelo.", 
+		cliente: "Cliente: Sole&Lorenzo", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Ideas Publicitarias ·  Montevideo", 
+		programas: "Programas: Illustrator y Photoshop", 
+		resumen: "Diseño Gráfico Sole&Lorenzo para prensa y diseños digitales." 
+	},
+	{ 
+		codigo:"thamato", 
+		fotos:"5", 
+		tituloportada: "<span>Thamato</span> · Diseño gráfico<br />Barcelona", 
+		titulo: "Diseño Gráfico catálogo", 
+		descripcion: "El diseño gráfico del catálogo de Thamato presenta una colección de camisetas femeninas de diseño únicas y exclusivas, creadas por diseñadores de moda en Barcelona. <br/> <br/>  Para reflejar la calidad y la creatividad de la marca, se ha creado un estilo elegante y moderno que destaca las características distintivas de cada camiseta. El catálogo utiliza una paleta de colores neutros que complementan los diseños únicos de las camisetas. Las imágenes de las camisetas se presentan en un estilo minimalista, con un enfoque en los detalles y las características especiales de cada diseño. Además, se han utilizado fondos sencillos y limpios para que la atención se centre en las camisetas.", 
+		cliente: "Cliente: Thamato", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Ideas Publicitarias ·  Montevideo", 
+		programas: "Programas: Indesign, Illustrator, Photoshop", 
+		resumen: "Diseño Gráfico Thamato para prensa y diseños digitales." 
+	},
+	{ 
+		codigo:"upc", 
+		fotos:"5", 
+		tituloportada: "<span>UPC</span> · Diseño Gráfico<br />Barcelona", 
+		titulo: "Diseño Gráfico UPC", 
+		descripcion: "El diseño del catálogo de la Universidad Politécnica de Cataluña (UPC) es una herramienta fundamental para informar a los estudiantes y visitantes sobre los diferentes cursos y programas que ofrece la universidad. <br /><br />El diseño gráfico se ha creado con el objetivo de transmitir la calidad y la excelencia de la universidad. El catálogo utiliza una combinación de colores planos que reflejan el espíritu innovador y tecnológico de la UPC. Además, se ha utilizado una tipografía clara y fácil de leer para que los estudiantes puedan navegar por el catálogo de manera sencilla. Cada sección del catálogo presenta información detallada sobre los cursos y programas, y se ha incluido imágenes de alta calidad que ilustran la vida universitaria y los programas de investigación.", 
+		cliente: "Cliente: Universidad Politécnica de Cataluña (UPC)", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: vinche studio lab", 
+		programas: "Programas: Indesign, Illustrator, Photoshop", 
+		resumen: "Diseño Gráfico UPC para promociones impresas." 
+	},
+	{ 
+		codigo:"victory", 
+		fotos:"3", 
+		tituloportada: "<span>Majestic Victory</span> · Diseño Textil<br />Barcelona", 
+		titulo: "Majestic originales téxtiles", 
+		descripcion: "El diseño textil para la marca Majestic inspirado en la estética de Victory (marca de motos) utiliza una combinación de colores rojo, negro y naranja para transmitir una sensación de potencia y velocidad. Las prendas creadas con este diseño se caracterizan por la combinación de elementos tipográficos de marca inspirados en las formas de las motocicletas. Los colores rojo, negro y naranja se utilizan para crear un contraste llamativo y dinámico que atrae la atención y refleja la intensidad de la experiencia de conducción. Los colores y elementos gráficos inspirados en las formas de las motocicletas, las prendas creadas con este diseño son una representación de la intensidad y la emoción de la experiencia de conducción pero para el diseño de ropa de baño femenina.", 
+		cliente: "Cliente: Majestic", 
+		anio: "Año: 2014", 
+		estudio: "Estudio: Vives Vidal Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "El diseño textil para la marca Majestic inspirado en la estética de Victory." 
+	},
+	{ 
+		codigo:"poster", 
+		fotos:"4", 
+		tituloportada: "<span>UDELAR · Ideas+</span> · Diseño Gráfico<br />Montevideo", 
+		titulo: "Ideas+ Diseño Gráfico", 
+		descripcion: "El diseño gráfico del poster para la feria Ideas+ en el Parque Rodó de Montevideo obtuvo el segundo puesto del certamen gracias a su originalidad, calidad y capacidad para transmitir la esencia del evento. El diseño del poster se basa en una paleta de colores que representan la naturaleza y el entorno y atractivos que reflejan la energía del entorno y la creatividad del evento. La técnica utilizada es el pincel que presenta un póster que atrae la atención del público.", 
+		cliente: "Cliente: IDEAS+", 
+		anio: "Año: 2022", 
+		estudio: "Estudio: FADU ·  Montevideo", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño gráfico del poster para la feria Ideas+." 
+	},
+	{ 
+		codigo:"nikegrafico", 
+		fotos:"7", 
+		tituloportada: "<span>Nike</span> · Diseño gráfico <br />Barcelona", 
+		titulo: "Nike diseño gráfico de catálogos", 
+		descripcion: "El diseño gráfico del catálogo de productos para Nike se ha creado con un enfoque minimalista y moderno, utilizando un esquema de color blanco para transmitir la sensación de simplicidad y elegancia. <br /><br />Cada producto se presenta con una ilustración vectorial detallada, que muestra las características y detalles clave de cada producto. Estas ilustraciones se han creado con gran atención al detalle para asegurar que el producto se vea atractivo y se entienda su funcionalidad. Además, se han utilizado fotografías frescas y vibrantes para ambientar el catálogo. Cada fotografía ha sido cuidadosamente seleccionada para complementar el producto y reflejar el estilo de vida de los consumidores de Nike.", 
+		cliente: "Cliente: Nike", 
+		anio: "Año: 2018", 
+		estudio: "Estudio: Vives Vidal Barcelona", 
+		programas: "Programas: Indesign, illustrator, Photoshop", 
+		resumen: "Diseño gráfico del catálogo de productos para Nike se ha creado con un enfoque minimalista."  
+	},
+	{ 
+		codigo:"galata", 
+		fotos:"3", 
+		tituloportada: "<span>Gálata</span> · Diseño gráfico<br />Oviedo", 
+		titulo: "Gálata diseño gráfico tríptico", 
+		descripcion: "El diseño gráfico del tríptico de productos para Galata ha sido creado con un enfoque limpio y elegante, utilizando colores institucionales y un fondo blanco para transmitir la seriedad y el profesionalismo de la empresa. Las imágenes utilizadas en el tríptico son cuidadosamente seleccionadas y se han editado de manera que se vean limpias y ordenadas, lo que refleja la calidad y el profesionalismo de los productos de Galata. Se ha utilizado una disposición coherente y equilibrada para asegurar que la información se presente de manera clara y ordenada. Este tríptico es una herramienta efectiva para promocionar y dar a conocer los eventos promocionales de Galicia.", 
+		cliente: "Cliente: Gálata", 
+		anio: "Año: 2019", 
+		estudio: "Estudio: IBCmass Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño gráfico del tríptico de productos para Galata."  
+	},
+	{ 
+		codigo:"accesibilidad", 
+		fotos:"3", 
+		tituloportada: "<span>Accesibilidad</span> · Diseño Gráfico<br />Oviedo", 
+		titulo: "Accesibilidad y Comotelocuento  diseño gráfico tríptico", 
+		descripcion: "El diseño gráfico del tríptico de productos para las Jornadas para la Accesibilidad en Gijón se ha creado con un enfoque fresco y moderno, utilizando colores vibrantes para transmitir la vitalidad y la energía de las jornadas. Las imágenes utilizadas en el tríptico son cuidadosamente seleccionadas para reflejar la diversidad y la inclusión que se promueven en las jornadas. <br /><br />La tipografía utilizada es moderna y fácil de leer, lo que garantiza que la información presentada sea clara y comprensible. En el caso del tríptico para la formación y la comunicación (TIC) de comotelocuento, también se ha utilizado un enfoque fresco y moderno con colores vibrantes para transmitir la vitalidad y el dinamismo de los cursos. <br /><br />La imágenes seleccionadas se han utilizado para reflejar la temática de los cursos y la tipografía utilizada es legible y moderna. Además, se ha utilizado una disposición equilibrada para asegurar que la información se presente de manera clara y ordenada.", 
+		cliente: "Cliente: Accesibilidad, Comotelocuento", 
+		anio: "Año: 2020", 
+		estudio: "Estudio: IBCmass Barcelona", 
+		programas: "Programas: Illustrator, Photoshop", 
+		resumen: "Diseño gráfico del tríptico de productos para las Jornadas para la Accesibilidad y Comotelocuento."  
+	},
+	
+
+];
